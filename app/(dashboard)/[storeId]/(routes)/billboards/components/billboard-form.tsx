c956e58f6841edd86147cb0b996fc24c034feb69
Guilherme Mattos <guilhermevsmattos@outlook.com>
2023-10-13 13:58:55 -0300

    feat: creating image upload

--- app/(dashboard)/[storeId]/(routes)/billboards/components/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/billboard-form.tsx
@@ -17,6 +17,7 @@ import axios from "axios";
 import { AlertModal } from "@/components/modals/alert-modal";
 import { ApiAlert } from "@/components/ui/api-alert";
 import { useOrigin } from "@/hooks/use-origin";
+import ImageUpload from "@/components/ui/image-upload";
 
 const formSchema = z.object({
   label: z.string().min(1),
@@ -106,6 +107,24 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
       <Separator />
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full ml-0">
+          <FormField 
+            control={form.control}
+            name="imageUrl"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Background image</FormLabel>
+                <FormControl>
+                  <ImageUpload 
+                    value={field.value ? [field.value] : []}
+                    disabled={loading}
+                    onChange={(url) => field.onChange(url)}
+                    onRemove={() => field.onChange("")}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
           <div className="grid grid-cols-3 gap-8">
             <FormField 
               control={form.control}
