12a54df60e0152a01443174435e24af222ffa6d5
Guilherme Mattos <guilhermevsmattos@outlook.com>
2023-10-13 16:42:46 -0300

    feat: adding routes to one billboard

--- app/(dashboard)/[storeId]/(routes)/billboards/components/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/billboard-form.tsx
@@ -57,11 +57,12 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
     try {
       setLoading(true);
       if (initialData) {
-        await axios.patch(`/api/${params.storeId}/billboards`, data);
+        await axios.patch(`/api/${params.storeId}/billboards/${params.billboardId}`, data);
       } else {
         await axios.post(`/api/${params.storeId}/billboards`, data);
       }
       router.refresh();
+      router.push(`/${params.storeId}/billboards`)
       toast.success(toastMessage);
     } catch (error) {
       toast.error("Something went wrong.")
